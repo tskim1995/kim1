c266476df8c8dc1123b809dc260bb83fd6fb6c54
unknown <tskim1995@naver.com>
2020-06-17 11:53:17 +0900

    Updates

---
+++ precache-manifest.8929470ff66747d3b5d1ab6707af0a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8c9a7ad9874a445fd6fa98bd371d21",
+    "url": "/kim1/index.html"
+  },
+  {
+    "revision": "629e5d0b180145a01865",
+    "url": "/kim1/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "09136f2b55364275aab1",
+    "url": "/kim1/static/js/2.cf620d40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kim1/static/js/2.cf620d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629e5d0b180145a01865",
+    "url": "/kim1/static/js/main.7d73ba46.chunk.js"
+  },
+  {
+    "revision": "e0b268e6cbc005af1880",
+    "url": "/kim1/static/js/runtime-main.e0230f9a.js"
+  }
+]);

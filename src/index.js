cf9c86d8a45d7131dbb4c210600dd94fa5e63850
unknown <tskim1995@naver.com>
2020-06-17 15:14:49 +0900

    all

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+// 로그인창
 class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -31,8 +33,8 @@ class Login extends React.Component {
         if(this.state.isLog){
             return(
                 <div className = 'login'>
-                    <h2>Welcome {this.state.value}</h2>
                     <form onSubmit = {this.handleInput}>
+                        <h2>Welcome {this.state.value}</h2>
                         <input type = 'submit' value = 'LogOut' />
                     </form>
                 </div>
@@ -53,30 +55,54 @@ class Login extends React.Component {
     }
 }
 
-
+// 제목
 class Title extends React.Component {
     render() {
         return(
-            <div>
+            <div className = 'title'>
                 <h1>Title</h1>
             </div>
         );
     }
 }
 
-class Page extends React.Component {
+
+function ListItem(props) {
+    return <li>{props.value}</li>;
+}
+
+// 리스트
+class List extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ''
-        };
+            List_value: ['a','b','c','d']
+        }
     }
 
+    render() {
+        const list_items = this.state.List_value;
+        const listItems = list_items.map((item) =>
+            <ListItem key = {item} value = {item} />
+            );
+
+        return(
+            <div className = 'section_list'>
+                <ul>
+                    {listItems}
+                </ul>
+            </div>
+        );
+    }
+}
+
+class Page extends React.Component {
     render() {
         return(
             <div className = 'Page'>
                 <Login />
                 <Title />
+                <List />
             </div>
         );
     }
